fe9a16acc9cd632019f45c9e70c37fb8d89f4f1c
hardik17771 <hardiksachdeva52@gmail.com>
2023-06-14 14:37:47 +0530

    Move zeroes completed

--- move_zeroes.cpp
+++ move_zeroes.cpp
@@ -1,20 +1,18 @@
  #include<iostream>
-#include<vector>
+ #include<vector>
 using namespace std;
- void moveZeroes(vector<int>& nums) {
-        int i,j,temp=0;
-        while(i<nums.size())
-        {
-            if(nums[j]==0)
+  void moveZeroes(vector<int>& nums) {
+       
+        int i=0;
+        for(int j=0;j<nums.size();j++)
+        {  
+        
+            if(nums[j]!=0)
             {
-              j++;
-            }
-            else
-            {
-               swap(nums[i],nums[j]);
+               swap(nums[j],nums[i]);
 
                i++;
-               j++;
+               
             }
         }
         for(int k=0;k<nums.size();++k)
